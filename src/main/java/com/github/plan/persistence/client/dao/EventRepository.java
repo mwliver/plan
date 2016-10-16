e7122e73867994307f87a9344abb1056223f3b2a
pwlgbb <pgraczek@gmail.com>
2016-10-16 21:04:50 +0200

    Dodanie zapytania do EventRepository

--- src/main/java/com/github/plan/persistence/client/dao/EventRepository.java
+++ src/main/java/com/github/plan/persistence/client/dao/EventRepository.java
@@ -1,6 +1,13 @@
 package com.github.plan.persistence.client.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Calendar;
+import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+    @Query("select e from #{#entityName} e where (e.timeFrom>=:timeFrom and e.timeFrom<=:timeTo) or (e.timeTo>=:timeFrom and e.timeTo<=:timeTo)")
+    List<Event> findPlannedOrOngoing(@Param("timeFrom") Calendar timeFrom, @Param("timeTo") Calendar timeTo);
 }
