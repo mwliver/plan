5eae988600e653392a53692ddceeb54b62174ced
michal <mw@coderion.pl>
2016-11-13 11:42:11 +0100

    EventController crud

--- src/main/java/com/github/plan/controller/EventController.java
+++ src/main/java/com/github/plan/controller/EventController.java
@@ -1,10 +1,7 @@
 package com.github.plan.controller;
 
 
-import com.github.plan.persistence.client.dao.Event;
-import com.github.plan.persistence.client.dao.Group;
-import com.github.plan.persistence.client.dao.Room;
-import com.github.plan.persistence.client.dao.User;
+import com.github.plan.persistence.client.dao.*;
 import com.github.plan.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,22 +11,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class EventController {
 
     @Autowired
     EventService eventService;
 
+    @Autowired
+    EventRepository eventRepository;
+
     @RequestMapping(value = "/event/add", method = RequestMethod.POST)
     public ResponseEntity<String> addEvent(@RequestBody Event event,
-                                           @RequestBody Group group,
+                                           @RequestBody Team team,
                                            @RequestBody Room room,
                                            @RequestBody User user) {
-        Boolean result = eventService.addEvent(event, group, room, user);
+        Boolean result = eventService.addEvent(event, team, room, user);
         if (Boolean.TRUE.equals(result)) {
             return new ResponseEntity<String>(HttpStatus.OK);
         } else {
             return new ResponseEntity<String>("Zajęte", HttpStatus.OK);
         }
+    }
+
+    @RequestMapping(value = "/event/list", method = RequestMethod.GET)
+    public List<Event> getEvents() {
+        return eventRepository.findAll();
+    }
+
+    @RequestMapping(value = "/event/save", method = RequestMethod.POST)
+    public ResponseEntity<String> saveEvent(@RequestBody Event event) {
+        eventRepository.save(event);
+        return new ResponseEntity<String>(HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/event/remove", method = RequestMethod.POST)
+    public ResponseEntity<String> removeEvent(@RequestBody Long eventId) {
+        eventRepository.delete(eventId);
+        return new ResponseEntity<String>(HttpStatus.OK);
     }
 }
