9b97ec9b0a5e522994b53fcf6cccc1551446937c
pwlgbb <pgraczek@gmail.com>
2016-12-11 15:02:56 +0100

    Metody do wyszukiwania po użytkowniku, pomieszczeniu i grupie w EventController

--- src/main/java/com/github/plan/controller/EventController.java
+++ src/main/java/com/github/plan/controller/EventController.java
@@ -6,12 +6,10 @@ import com.github.plan.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class EventController {
@@ -38,6 +36,27 @@ public class EventController {
     @RequestMapping(value = "/event/list", method = RequestMethod.GET)
     public List<Event> getEvents() {
         return eventRepository.findAll();
+    }
+
+    @RequestMapping(value = "/event/list/user/{userId}", method = RequestMethod.GET)
+    public List<Event> getEventsByUserId(@PathVariable("userId") Long userId) {
+        List<Event> events = eventRepository.findAll();
+
+        return events.stream().filter(n -> n.getUser().getId() == userId).collect(Collectors.toList());
+    }
+
+    @RequestMapping(value = "/event/list/team/{teamId}", method = RequestMethod.GET)
+    public List<Event> getEventsByTeamId(@PathVariable("teamId") Long teamId) {
+        List<Event> events = eventRepository.findAll();
+
+        return events.stream().filter(n -> n.getTeam().getId() == teamId).collect(Collectors.toList());
+    }
+
+    @RequestMapping(value = "/event/list/room/{roomId}", method = RequestMethod.GET)
+    public List<Event> getEventsByRoomId(@PathVariable("roomId") Long roomId) {
+        List<Event> events = eventRepository.findAll();
+
+        return events.stream().filter(n -> n.getRoom().getId() == roomId).collect(Collectors.toList());
     }
 
     @RequestMapping(value = "/event/save", method = RequestMethod.POST)
